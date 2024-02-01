708689b3dd4567ae7cb6810fa29ebca26835f477
elias <elias@Ubuntu22.04.3.myguest.virtualbox.org>
2024-02-01 16:08:15 +0100

    Main a bit more Norm compliant.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: efret <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 12:39:58 by efret             #+#    #+#             */
-/*   Updated: 2024/02/01 13:09:49 by elias            ###   ########.fr       */
+/*   Updated: 2024/02/01 16:04:21 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,18 @@
 
 int	main(int argc, char **argv)
 {
+	t_stacks	*stacks;
+
 	if (argc < 2)
 	{
 		ft_printf("Give me some arguments.\n"); // delete later.
 		return (0);
 	}
-
-	t_stacks	*stacks;
-
 	stacks = ft_init_stacks();
 	ft_check_input(stacks, argc - 1, &argv[1]);
 	ft_display_stacks(stacks);
 	if (ft_check_sorted(stacks))
-		ft_printf("Good job sorting!\nWait a minute... The input was already sorted!\n");
+		ft_printf("The input was already sorted!\n");
 	else
 		ft_stacks_interactive(stacks);
 	ft_del_all(&stacks);
