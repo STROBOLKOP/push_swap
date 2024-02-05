2544bd23781a8ff9a64b4aed1122953e432652d5
Elias Fret <efret@student.s19.be>
2024-02-05 15:51:32 +0100

    Added general push function.
    Push to a destination stack from a source stack.
    
    At the moment it just uses ft_stack_front and ft_stack_del_front. Just changing where the pointers point to was a bit too long for the norm in my first implementation. Maybe if I don't use a member last in the stack.

--- src/commands/ft_push_commands.c
+++ src/commands/ft_push_commands.c
@@ -6,27 +6,32 @@
 /*   By: elias <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 13:09:16 by elias             #+#    #+#             */
-/*   Updated: 2024/02/01 16:14:53 by elias            ###   ########.fr       */
+/*   Updated: 2024/02/05 15:46:15 by efret            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../libft/incl/libft.h"
 #include "../../push_swap.h"
 
+int	ft_stack_push(t_stack *dest, t_stack *src)
+{
+	if (!dest || !src)
+		return (E_UNEXPECTED_ERROR);
+	if (!src->len)
+		return (E_SUCCES);
+	ft_stackadd_front(dest, src->head->value);
+	ft_stackdel_front(src);
+	return (E_SUCCES);
+}
+
 void	ft_stacks_pa(t_stacks *stacks)
 {
-	if (!stacks->a || !stacks->b || !stacks->b->len)
-		return ;
-	ft_stackadd_front(stacks->a, stacks->b->head->value);
-	ft_stackdel_front(stacks->b);
+	ft_stack_push(stacks->a, stacks->b);
 	ft_printf("pa\n");
 }
 
 void	ft_stacks_pb(t_stacks *stacks)
 {
-	if (!stacks->a || !stacks->b || !stacks->a->len)
-		return ;
-	ft_stackadd_front(stacks->b, stacks->a->head->value);
-	ft_stackdel_front(stacks->a);
+	ft_stack_push(stacks->b, stacks->a);
 	ft_printf("pb\n");
 }
