0ef77293af355f94302f09a8cab1f14c4318b016
Elias Fret <efret@student.s19.be>
2024-02-07 13:14:40 +0100

    push_swap now checks if input is sorted before solving.

--- src/push_swap_main.c
+++ src/push_swap_main.c
@@ -6,7 +6,7 @@
 /*   By: efret <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 12:39:58 by efret             #+#    #+#             */
-/*   Updated: 2024/02/05 17:28:29 by efret            ###   ########.fr       */
+/*   Updated: 2024/02/07 13:04:41 by efret            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,8 @@ int	main(int argc, char **argv)
 	ft_check_input(stacks, argc - 1, &argv[1]);
 	if (ft_rank_stack(stacks->a))
 		ft_exit_handler(stacks, E_UNEXPECTED_ERROR);
-	ft_simplest_solve(stacks);
+	if (!ft_check_sorted(stacks))
+		ft_simplest_solve(stacks);
 	ft_del_all(&stacks);
 	return (0);
 }
