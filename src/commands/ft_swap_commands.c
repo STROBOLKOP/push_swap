87b2338ab158c1ee3fdb44c9cd4884cc0ea677b6
Elias Fret <efret@student.s19.be>
2024-02-07 16:52:43 +0100

    Forgot to swap the rank as well.

--- src/commands/ft_swap_commands.c
+++ src/commands/ft_swap_commands.c
@@ -6,35 +6,36 @@
 /*   By: elias <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 13:04:58 by elias             #+#    #+#             */
-/*   Updated: 2024/02/05 15:06:17 by efret            ###   ########.fr       */
+/*   Updated: 2024/02/07 16:38:24 by efret            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../libft/incl/libft.h"
 #include "../../push_swap.h"
 
 int	ft_stack_swap(t_stack *stack)
 {
-	int tmp;
+	int		tmp;
+	size_t	tmp_rank;
 
 	if (!stack || stack->len <= 1)
 		return (E_UNEXPECTED_ERROR);
 	tmp = stack->head->value;
 	stack->head->value = stack->head->next->value;
 	stack->head->next->value = tmp;
+	tmp_rank = stack->head->rank;
+	stack->head->rank = stack->head->next->rank;
+	stack->head->next->rank = tmp_rank;
 	return (E_SUCCES);
 }
 
 void	ft_stacks_sa(t_stacks *stacks)
 {
 	ft_stack_swap(stacks->a);
-	ft_printf("sa\n");
 }
 
 void	ft_stacks_sb(t_stacks *stacks)
 {
 	ft_stack_swap(stacks->b);
-	ft_printf("sb\n");
 }
 
 void	ft_stacks_ss(t_stacks *stacks)
